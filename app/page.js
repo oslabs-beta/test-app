e66fa7c7f2eda9e14cccee6e8cc8c7b2eb9bb101
Tom Nguyen <tomtnguyen88@gmail.com>
2023-06-26 14:00:05 -0700

    base page for test app renders

--- app/page.js
+++ app/page.js
@@ -1,95 +1,59 @@
 import Image from 'next/image'
+import Link from "next/link";
 import styles from './page.module.css'
+import { revalidatePath } from 'next/cache';
 
-export default function Home() {
+const moves = ['Halo to Hollow'];
+
+const MoveBox = () => {
+  async function addMove(data) {
+    "use server"
+    const move = data.get('move');
+    moves.push(move);
+    revalidatePath('/');
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+      <h1 className={styles.boxTitle}>Best Bboy Blog on the Web</h1>
+      <div className={styles.moveBox}>
+        <h1 className={styles.boxTitle}>Moves/Creations</h1>
+        <ul>
+          {moves.map((move, i) => (
+            <li key={i}>{move}</li>
+          ))}
+        </ul>
+
+        <form action={addMove}>
+          <input
+            type="text"
+            name="move"
+            className={styles.textBox}
+          />
+          <button
+            type="submit"
+            className={styles.button}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+            Add move
+          </button>
+        </form>
       </div>
     </main>
+
   )
+}
+
+
+
+export default function Home() {
+
+
+  return (
+    <>
+      <MoveBox />
+
+    </>
+
+
+  );
 }
